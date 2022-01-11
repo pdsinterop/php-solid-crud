0539993dc91781083d66c8b6bc3bba8e04e32985
Ben Peachey <ben@muze.nl>
2022-01-11 09:49:30 +0100

    Change Server class with non-functional code cleanup
    - Sort class constants alphabetically
    - Move getter and setters above constructor
    - Whitespace fixes

--- src/Server.php
+++ src/Server.php
@@ -11,15 +11,16 @@ class Server
 {
     ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+	public const ERROR_CAN_NOT_PARSE_FOR_PATCH = 'Could not parse the requested resource for patching';
     public const ERROR_CAN_NOT_DELETE_NON_EMPTY_CONTAINER = 'Only empty containers can be deleted, "%s" is not empty';
     public const ERROR_NOT_IMPLEMENTED_SPARQL = 'SPARQL Not Implemented';
     public const ERROR_PATH_DOES_NOT_EXIST = 'Requested path "%s" does not exist';
     public const ERROR_PATH_EXISTS = 'Requested path "%s" already exists';
     public const ERROR_POST_EXISTING_RESOURCE = 'Requested path "%s" already exists. Can not "POST" to existing resource. Use "PUT" instead';
-    public const ERROR_PUT_NON_EXISTING_RESOURCE = self::ERROR_PATH_DOES_NOT_EXIST . '. Can not "PUT" non-existing resource. Use "POST" instead';
     public const ERROR_PUT_EXISTING_RESOURCE = self::ERROR_PATH_EXISTS . '. Can not "PUT" existing container.';
+    public const ERROR_PUT_NON_EXISTING_RESOURCE = self::ERROR_PATH_DOES_NOT_EXIST . '. Can not "PUT" non-existing resource. Use "POST" instead';
     public const ERROR_UNKNOWN_HTTP_METHOD = 'Unknown or unsupported HTTP METHOD "%s"';
-	public const ERROR_CAN_NOT_PARSE_FOR_PATCH = 'Could not parse the requested resource for patching';
+
     private const MIME_TYPE_DIRECTORY = 'directory';
     private const QUERY_PARAM_HTTP_METHOD = 'http-method';
 
@@ -33,51 +34,71 @@ class Server
         'POST',
         'PUT',
     ];
+    /** @var string */
+    private $baseUrl;
     /** @var Filesystem */
     private $filesystem;
+    /** @var string */
+    private $pubsub;
     /** @var Response */
     private $response;
-	private $pubsub;
-	private $baseUrl;
+
+    //////////////////////////// GETTERS AND SETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    final public function getFilesystem()
+    {
+        return $this->filesystem;
+    }
+
+    final public function getResponse()
+    {
+        return $this->response;
+    }
+
+    final public function setBaseUrl($url)
+    {
+        $this->baseUrl = $url;
+
+        $serverRequest = new \Laminas\Diactoros\ServerRequest(array(),array(), $this->baseUrl);
+        $this->basePath = $serverRequest->getUri()->getPath();
+    }
+
+    final public function setPubSubUrl($url)
+    {
+        $this->pubsub = $url;
+    }
 
     //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     final public function __construct(Filesystem $filesystem, Response $response)
     {
+        $this->basePath = '';
+        $this->baseUrl = '';
+        $this->pubsub = '';
         $this->filesystem = $filesystem;
         $this->response = $response;
-		$this->pubsub = '';
-		$this->baseUrl = '';
-		$this->basePath = '';
     }
 
-	final public function getFilesystem() {
-		return $this->filesystem;
-	}
-	final public function getResponse() {
-		return $this->response;
-	}
-
-    final public function respondToRequest(Request $request) : Response
+    final public function respondToRequest(Request $request): Response
     {
         $path = $request->getUri()->getPath();
 		if ($this->basePath) {
 			$path = str_replace($this->basePath, "", $path);
 		}
-	$path = rawurldecode($path);
-	
+		$path = rawurldecode($path);
+
 		// @FIXME: The path can also come from a 'Slug' header
 
         $method = $this->getRequestMethod($request);
 
         $contents = $request->getBody()->getContents();
-		
+
         return $this->handle($method, $path, $contents, $request);
     }
 
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    private function getRequestMethod(Request $request) : string
+    private function getRequestMethod(Request $request): string
     {
         $method = $request->getMethod();
 
@@ -93,18 +114,7 @@ class Server
         return $method;
     }
 
-	public function setPubSubUrl($url) {
-		$this->pubsub = $url;
-	}
-	public function setBaseUrl($url) {
-		$this->baseUrl = $url;
-
-		$uri = $this->baseUrl;
-		$serverRequest = new \Laminas\Diactoros\ServerRequest(array(),array(), $this->baseUrl);
-		$this->basePath = $serverRequest->getUri()->getPath();
-	}
-
-    private function handle(string $method, string $path, $contents, $request) : Response
+    private function handle(string $method, string $path, $contents, $request): Response
     {
         $response = $this->response;
         $filesystem = $this->filesystem;
@@ -177,10 +187,10 @@ class Server
 							$filename = $slug;
 						} else {
 							$filename = $this->guid();
-						}						
+						}
 						// FIXME: make this list complete for at least the things we'd expect (turtle, n3, jsonld, ntriples, rdf);
 						// FIXME: if no content type was passed, we should reject the request according to the spec;
-						
+
 						switch ($contentType) {
 							case "text/plain":
 								$filename .= ".txt";
@@ -229,7 +239,7 @@ class Server
         return $response;
     }
 
-	private function handleSparqlUpdate(Response $response, string $path, $contents) : Response
+	private function handleSparqlUpdate(Response $response, string $path, $contents): Response
 	{
         $filesystem = $this->filesystem;
 		$graph = new \EasyRdf_Graph();
@@ -243,7 +253,7 @@ class Server
 
 		try {
 			// Assuming this is in our native format, turtle
-			$graph->parse($data, "turtle"); 
+			$graph->parse($data, "turtle");
 			// FIXME: Adding this base will allow us to parse <> entries; , $this->baseUrl . $this->basePath . $path), but that breaks the build.
 			// FIXME: Use enums from namespace Pdsinterop\Rdf\Enum\Format instead of 'turtle'?
 
@@ -314,7 +324,7 @@ class Server
 		return $response;
 	}
 
-    private function handleCreateRequest(Response $response, string $path, $contents) : Response
+    private function handleCreateRequest(Response $response, string $path, $contents): Response
     {
         $filesystem = $this->filesystem;
 
@@ -336,7 +346,9 @@ class Server
 
         return $response;
 	}
-	private function parentPath($path) {
+
+	private function parentPath($path)
+	{
 		if ($path == "/") {
 			return "/";
 		}
@@ -347,8 +359,8 @@ class Server
 		}
 		return implode("/", $pathicles) . "/";
 	}
-	
-    private function handleCreateDirectoryRequest(Response $response, string $path) : Response
+
+    private function handleCreateDirectoryRequest(Response $response, string $path): Response
     {
         $filesystem = $this->filesystem;
         if ($filesystem->has($path) === true) {
@@ -366,12 +378,13 @@ class Server
         return $response;
 	}
 
-	private function sendWebsocketUpdate($path) {
+	private function sendWebsocketUpdate($path)
+    {
 		$pubsub = $this->pubsub;
 		if (!$pubsub) {
 			return; // no pubsub server available, don't even try;
 		}
-		
+
 		$pubsub = str_replace("https://", "ws://", $pubsub);
 		$pubsub = str_replace("http://", "ws://", $pubsub);
 
@@ -389,8 +402,8 @@ class Server
 			$client->send("pub $baseUrl$path\n");
 		}
 	}
-	
-    private function handleDeleteRequest(Response $response, string $path, $contents) : Response
+
+    private function handleDeleteRequest(Response $response, string $path, $contents): Response
     {
         $filesystem = $this->filesystem;
 
@@ -429,7 +442,7 @@ class Server
         return $response;
     }
 
-    private function handleUpdateRequest(Response $response, string $path, string $contents) : Response
+    private function handleUpdateRequest(Response $response, string $path, string $contents): Response
     {
         $filesystem = $this->filesystem;
 
@@ -448,7 +461,8 @@ class Server
         return $response;
     }
 
-	private function getRequestedMimeType($accept) {		
+	private function getRequestedMimeType($accept)
+    {
 		// text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
 		$mimes = explode(",", $accept);
 		foreach ($mimes as $mime) {
@@ -461,10 +475,11 @@ class Server
 					return $mimeInfo;
 				break;
 			}
-		}			
+		}
 		return '';
 	}
-    private function handleReadRequest(Response $response, string $path, $contents, $mime='') : Response
+
+    private function handleReadRequest(Response $response, string $path, $contents, $mime=''): Response
     {
 		$filesystem = $this->filesystem;
 		if ($path == "/") { // FIXME: this is a patch to make it work for Solid-Nextcloud; we should be able to just list '/';
@@ -472,7 +487,7 @@ class Server
 			$response->getBody()->write($contents);
 			$response = $response->withHeader("Content-type", "text/turtle");
 			$response = $response->withStatus(200);
-		} else if ($filesystem->has($path) === false) {			
+		} else if ($filesystem->has($path) === false) {
             $message = vsprintf(self::ERROR_PATH_DOES_NOT_EXIST, [$path]);
             $response->getBody()->write($message);
             $response = $response->withStatus(404);
@@ -509,12 +524,14 @@ class Server
 
         return $response;
     }
-	
-	private function guid() {
+
+	private function guid()
+    {
 		return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
 	}
 
-	private function listDirectoryAsTurtle($path) {
+	private function listDirectoryAsTurtle($path)
+    {
         $filesystem = $this->filesystem;
 		if ($path == "/") {
 			$listContents = $filesystem->listContents(".");// FIXME: this is a patch to make it work for Solid-Nextcloud; we should be able to just list '/';
@@ -523,7 +540,7 @@ class Server
 		}
 		// CHECKME: maybe structure this data als RDF/PHP
 		// https://www.easyrdf.org/docs/rdf-formats-php
-		
+
 		$name = basename($path) . ":";
 		// turtle syntax doesn't allow labels that start with a number, so prefix it if it does;
 		if (preg_match("/^\d/", $name)) {
@@ -535,7 +552,7 @@ class Server
 				"ldp:contains" => array()
 			)
 		);
-		
+
 		foreach ($listContents as $item) {
 			switch($item['type']) {
 				case "file":
@@ -547,7 +564,7 @@ class Server
 				break;
 				case "dir":
 					// FIXME: we have a trailing slash here to please the test suits, but it probably should also pass without it since we are a Container.
-					$filename = "<" . rawurlencode($item['basename']) . "/>"; 
+					$filename = "<" . rawurlencode($item['basename']) . "/>";
 					$turtle[$filename] = array(
 						"a" => array("ldp:BasicContainer", "ldp:Container", "ldp:Resource")
 					);
@@ -573,7 +590,7 @@ EOF;
 					$lines[] = "\t" . $property . " " . implode(", ", $values);
 				}
 			}
-			
+
 			$container .= implode(";\n", $lines);
 			$container .= ".\n";
 		}

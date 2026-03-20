4e27161bb819cfbab9bf49251bb5c267af878dd3
Ben Peachey <potherca@gmail.com>
2026-03-20 15:46:02 +0100

    Add empty file for Server unit-tests.

---
+++ tests/unit/ServerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Unit Test for the Server class
+ */
+namespace Pdsinterop\Solid\Resources;
+
+use PHPUnit\Framework\TestCase;
+
+class ServerTest extends TestCase
+{
+    /**
+     * @coversNothing
+     */
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}

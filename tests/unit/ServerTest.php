59879ea490c1c4672ad50ca5a3462e5e221e9361
Ben Peachey <potherca@gmail.com>
2026-03-20 16:02:28 +0100

    Add unit-test for Server constructor.

--- tests/unit/ServerTest.php
+++ tests/unit/ServerTest.php
@@ -4,15 +4,60 @@
  */
 namespace Pdsinterop\Solid\Resources;
 
+use ArgumentCountError;
+use EasyRdf\Graph;
+use League\Flysystem\FilesystemInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
+/**
+ * @coversDefaultClass \Pdsinterop\Solid\Resources\Server
+ * @covers ::__construct
+ */
 class ServerTest extends TestCase
 {
-    /**
-     * @coversNothing
-     */
-    public function testDummy(): void
+    /** @testdox Server should complain when instantiated without File System */
+    public function testInstatiationWithoutFileSystem()
     {
-        $this->assertTrue(true);
+        $this->expectException(ArgumentCountError::class);
+        $this->expectExceptionMessageMatches('/Too few arguments .+ 0 passed/');
+
+        new Server();
+    }
+
+    /** @testdox Server should complain when instantiated without Response */
+    public function testInstatiationWithoutResponse()
+    {
+        $this->expectException(ArgumentCountError::class);
+        $this->expectExceptionMessageMatches('/Too few arguments .+ 1 passed/');
+
+        $mockFileSystem = $this->getMockBuilder(FilesystemInterface::class)->getMock();
+
+        new Server($mockFileSystem);
+    }
+
+    /** @testdox Server should be instantiated when constructed without Graph */
+    public function testInstatiationWithoutGraph()
+    {
+        $mockFileSystem = $this->getMockBuilder(FilesystemInterface::class)->getMock();
+        $mockResponse = $this->getMockBuilder(ResponseInterface::class)->getMock();
+
+        $actual = new Server($mockFileSystem, $mockResponse);
+        $expected = Server::class;
+
+        $this->assertInstanceOf($expected, $actual);
+    }
+
+    /** @testdox Server should be instantiated when constructed with Graph */
+    public function testInstatiationWithGraph()
+    {
+        $mockFileSystem = $this->getMockBuilder(FilesystemInterface::class)->getMock();
+        $mockResponse = $this->getMockBuilder(ResponseInterface::class)->getMock();
+        $mockGraph = $this->getMockBuilder(Graph::class)->getMock();
+
+        $actual = new Server($mockFileSystem, $mockResponse, $mockGraph);
+        $expected = Server::class;
+
+        $this->assertInstanceOf($expected, $actual);
     }
 }
